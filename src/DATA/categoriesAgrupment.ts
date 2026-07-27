52416175c4cd249f4e738a98e357c9d417002f99
PecelisAgustin <agustin.pecelis@gmail.com>
2026-07-27 20:18:57 -0300

    arreglando categorias

--- src/DATA/categoriesAgrupment.ts
+++ src/DATA/categoriesAgrupment.ts
@@ -4,12 +4,15 @@ import { cantantes } from "../categories/cantantes";
 import { capitals } from "../categories/capitales";
 import { comidas } from "../categories/comidas";
 import { deportistas } from "../categories/deportistas";
-import { escudosFutbol } from "../categories/Equipos futbol";
+import { escudosFutbol } from "../categories/Equiposfutbol";
 import { frasesConocidas } from "../categories/frases";
 import { englishWords } from "../categories/ingles";
 import { logos } from "../categories/logos";
 import { mathQuestions } from "../categories/matematica";
-import { peliculasEsteSiglo } from "../categories/peliculasEsteSiglo";
+import { peliculas1920a1969 } from "../categories/peliculas1920a1969"; 
+import { peliculas1970a1990 } from "../categories/peliculas1970a1990";
+import { peliculas1990a2009 } from "../categories/peliculas1990a2009";
+import { peliculas2010aActualidad } from "../categories/peliculas2010aActualidad";
 import { tablaPeriodica } from "../categories/tablaPeriodica";
 import { portugueseWords } from "../categories/portugues";
 import { animatedMovieQuestions } from "../categories/peliculasAnimadas";
@@ -29,7 +32,6 @@ import { geometria } from "../categories/geometria";
 import { numerosRomanos } from "../categories/romanos";
 import { sucesiones } from "../categories/sucesiones";
 import { cuerpo } from "../categories/cuerpo";
-import { peliculasSigloPasado } from "../categories/peliculasSigloPasado";
 import { canciones2020s } from "../categories/canciones2020s";
 import { cancionesPeliculas } from "../categories/cancionesPeliculas";
 import { ecuaciones } from "../categories/ecuaciones";
@@ -70,8 +72,10 @@ export const categoryQuestions: CategoryGroup[] = [
     ["Series", series],
     ["Libros por emojis", librosPorEmojis],
     ["Actores/Directores", actores],
-    ["Peliculas siglo XXI", peliculasEsteSiglo],
-    ["Peliculas siglo XX", peliculasSigloPasado],
+    ["Peliculas 1920-1969", peliculas1920a1969],
+    ["Peliculas 1970-1990", peliculas1970a1990],
+    ["Peliculas 1991-2009", peliculas1990a2009],
+    ["Peliculas 2010-2026", peliculas2010aActualidad],
     ["Peliculas animadas", animatedMovieQuestions],
     ["Canciones peliculas", cancionesPeliculas],
     ["Musica 80s o antes", canciones80s],
